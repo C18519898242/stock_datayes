b72f9ffa2b881d52174bd4b27b6b47673b49ebf0
yumiao <18611301980@163.com>
2023-10-04 22:12:43 +0800

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# stock_datayes
+# stock_data_yes
+
+
+ pip install git+https://github.com/C18519898242/stock_datayes.git@main
--- datayes/__init__.py
+++ datayes/__init__.py
@@ -1,7 +1,5 @@
-from commons.logger import get_logger
-from commons.stock_info import download_stock_info
+from stock_info import download_stock_info
 
 __all__ = (
-    "get_logger",
     "download_stock_info"
 )
